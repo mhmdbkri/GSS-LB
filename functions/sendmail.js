2c4ce62efabb4b4b3a0f277c223eb07efd8a1285
mohyddintash <mohyddin.tash@gmail.com>
2021-03-04 01:05:37 +0200

    add serverless function for email sending

---
+++ functions/sendmail.js
@@ -0,0 +1,36 @@
+// https://app.netlify.com/sites/vibrant-goodall-c5cf08/settings/domain#https
+// https://css-tricks.com/netlify-functions-for-sending-emails/
+// https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget/
+// https://rajrajhans.com/2020/07/using-netlify-lambda-functions-and-sendgrid-to-send-mail/
+// https://support.sendgrid.com/hc/en-us?return_to=%2Fhc%2Frequests
+
+// functions/sendmail.js
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+exports.handler = async (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  const { email, subject } = data;
+  const body = Object.keys(data)
+    .map((k) => {
+      return `${k}: ${data[k]}`;
+    })
+    .join("<br><br>");
+  const mail_to_send = {
+    to: "mohyddintash@gmail.com",
+    from: email,
+    subject: subject ? subject : "New Message from Contact Form",
+    html: body,
+  };
+  try {
+    await sgMail.send(mail_to_send);
+    return {
+      statusCode: 200,
+      body: "Message sent successfully",
+    };
+  } catch (e) {
+    return {
+      statusCode: e.code,
+      body: e.message,
+    };
+  }
+};
